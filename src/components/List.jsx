2ac638bbdf3804981a5aa82d42076341c27584bf
NaHyeon647 <forzue@sookmyung.ac.kr>
2025-06-26 15:09:39 +0900

    ItemList 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,7 +9,7 @@ const List = () => {
             <Button text={"편집"} type={"edit"} />
             <div className="place">거실</div>
             <div className="attribute">{`프로필 \u00A0\u00A0\u00A0\u00A0to-clean  \u00A0\u00A0\u00A0\u00A0마감 기한`}</div>
-            <div className="ListItem">
+            <div>
                 <ListItem />
             </div>
         </div>
