6741b5bc6893c4a0eab4f29112cbfd705b6a3993
Nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-01 07:00:25 +0900

    hover 버튼으로 구현 중

--- src/components/CheckList/ListAddModal.jsx
+++ src/components/CheckList/ListAddModal.jsx
@@ -3,17 +3,40 @@ import { useContext } from "react";
 import Modal from "../Modal";
 import Button from "../Button";
 
-import { toCleanStateContext } from "../../App";
-import { toCleanDispatchContext } from "../../App";
+import { toCleanStateContext, toCleanDispatchContext } from "../../App";
 import { getBadgeImage } from "../../utils/get-badge-images";
 import DatePicker from "react-datepicker";
 
 const ListAddModal = ({ isAddMode, setIsAddMode, selectedPlace }) => {
     const { onCreate } = useContext(toCleanDispatchContext);
     const { personData } = useContext(toCleanStateContext);
+    const addMockData = {};
+    // target, name, badgeId, place, toClean, deadLine
+    // target: "group",
+    // id: 1,
+    // name: "A",
+    // badgeId: 1,
+    // place: "거실",
+    // toClean: "tv 닦기",
+    // deadLine: "D-2",
 
     const onClickCloseModal = () => {
         setIsAddMode(false);
+    };
+
+    const onKeyDownToClean = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addMockData["toClean"] = e.target.value;
+        }
+    };
+    // return 문 안에 못 쓰는거 정리하기
+    const onChangeDeadLine = (e) => {
+        addMockData["deadLine"] = "D-test"; // e.target.value; (아직 DatePicker 구현 안해서 임시로 넣어둠)
+    };
+
+    const onClickSave = () => {
+        return <>{onCreate()}</>;
     };
 
     return (
@@ -39,13 +62,14 @@ const ListAddModal = ({ isAddMode, setIsAddMode, selectedPlace }) => {
                     <div className="toClean_text">
                         추가하실 to-clean을 입력하세요.
                     </div>
-                    <textarea></textarea>
+                    <textarea onKeyDown={onKeyDownToClean}></textarea>
                 </section>
                 <section className="createDeadLine">
                     <div className="deadLine_text">마감 기한</div>
                     <DatePicker
                         className="DatePicker"
                         placeholderText="0000-00-00"
+                        onChange={onChangeDeadLine}
                     />
                 </section>
                 <section className="selectPerson">
@@ -53,18 +77,18 @@ const ListAddModal = ({ isAddMode, setIsAddMode, selectedPlace }) => {
                     <div className="personTodo">
                         {personData.map((item) => {
                             return (
-                                <div className="hover_wrapper">
+                                <button className="hover_wrapper">
                                     <img
                                         className="BadgeTodo"
                                         src={getBadgeImage(item.badgeId)}
                                     />
                                     <div className="nameTodo">{item.name}</div>
-                                </div>
+                                </button>
                             );
                         })}
                     </div>
                 </section>
-                <Button type={"save"} text={"저장"} />
+                <Button onClick={onClickSave} type={"save"} text={"저장"} />
             </Modal>
         </div>
     );
