4aa8f894202d51035641ca33accde9f7f5ffb892
Nahyeon647 <forzeu@sookmyung.ac.kr>
2025-07-04 01:06:29 +0900

    체크리스트 G, P 기능 구현 완료3

--- src/pages/GroupHomePage.jsx
+++ src/pages/GroupHomePage.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import GroupHome from "../components/GroupHome/GroupHome";
+// import GroupHome from "../components/GroupHome/GroupHome";
 
 const GroupHomePage = () => {
     return (
